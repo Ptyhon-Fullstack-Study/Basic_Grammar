0b8458745c38b9652bffbd927c922dee2739e837
imchanyang <cksdid636@naver.com>
2023-09-10 19:01:27 +0900

    study about dictionary data type

---
+++ chanyang/02_data_type/05_dictionary.py
@@ -0,0 +1,33 @@
+dic = {'name': 'pey', 'phone': '010-9999-1234', 'birth': '1118'}
+print(dic)
+
+a = {1: 'a'}
+print(a)
+a[2] = 'b'
+print(a)
+
+a['name'] = 'pey'
+print(a)
+print(a[1])
+
+a = {1:'a', 1:'b'}
+print(a)
+
+a = {'name': 'pey', 'phone': '010-9999-1234', 'birth': '1118'}
+print(a.keys())
+print(list(a.keys()))
+
+print(a.values())
+print(list(a.values()))
+
+print(a.items())
+
+a.clear()
+print(a)
+
+a = {'name': 'pey', 'phone': '010-9999-1234', 'birth': '1118'}
+print(a.get('name'))
+print(a.get(1))
+
+print('name' in a)
+print(1 in a)
