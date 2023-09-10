70f1575573859ea3deee35df2ce4d7694fc878bb
imchanyang <cksdid636@naver.com>
2023-09-10 18:16:17 +0900

    study about number data type

--- chanyang/02_data_type/01_number.py
+++ chanyang/02_data_type/01_number.py
@@ -1,5 +1,32 @@
 a = 123
 print(a)
-
 a = -178
 print(a)
+
+a = 1.2
+print(a)
+a = -3.45
+print(a)
+
+a = 4.24E10
+print(a)
+a = 4.24e-10
+print(a)
+
+a = 0b100
+print(a)
+a = 0o177
+print(a)
+a = 0x8ff
+print(a)
+
+a = 3
+b = 4
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b)
+print(a % b)
+print(a // b)
+
