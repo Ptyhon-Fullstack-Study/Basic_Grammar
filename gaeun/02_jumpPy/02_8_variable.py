a3d1f83ddd42339bd5f6d9f978515870fb7e47ed
gaeunpark7 <hen77360@gmail.com>
2023-10-11 01:28:18 +0900

    Delete gaeun directory

--- gaeun/02_jumpPy/02_8_variable.py
+++
@@ -1,5 +0,0 @@
-from copy import copy
-a=[1,2,3]
-b=copy(a)
-
-print(b is a)
--- gaeun/05_jumpPy/05_3_packages.py
+++
@@ -1,5 +0,0 @@
-C:/doit/game/__init__.py
-C:/doit/game/sound/__init__.py
-C:/doit/game/sound/echo.py
-C:/doit/game/graphic/__init__.py
-C:/doit/game/graphic/render.py
